0c78d95560b39fbb6268cd6ecafe31e5c0d85a47
Shubham Negi <shubham64negi@gmail.com>
2024-04-21 15:51:48 +0530

    made the website functional

--- app.py
+++ app.py
@@ -1,11 +1,133 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
-from cvzone.HandTrackingModule import HandDetector
 import cv2
+from flask import Response
+import threading
 import numpy as np
+from cvzone.HandTrackingModule import HandDetector
 
 app = Flask(__name__)
 
+# Parameters
+width, height = 1280, 720
+gestureThreshold = 300
+folderPath = "presentation"
+
+# Camera Setup
+cap = cv2.VideoCapture(0)
+cap.set(3, width)
+cap.set(4, height)
+
+# Hand Detector
+detectorHand = HandDetector(detectionCon=0.8, maxHands=1)
+
+# Variables
+imgList = []
+delay = 30
+buttonPressed = False
+counter = 0
+drawMode = False
+imgNumber = 0
+delayCounter = 0
+annotations = [[]]
+annotationNumber = -1
+annotationStart = False
+hs, ws = int(120 * 1), int(213 * 1)  # width and height of small image
+
+# Get list of presentation images
+pathImages = sorted(os.listdir(folderPath), key=len)
+
+def generate_frames():
+    global buttonPressed, annotations, counter
+    imgNumber = 0 
+    while True:
+        # Get image frame
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        pathFullImage = os.path.join(folderPath, pathImages[imgNumber])
+        imgCurrent = cv2.imread(pathFullImage)
+
+        # Find the hand and its landmarks
+        hands, img = detectorHand.findHands(img)  # with draw
+
+        # Draw Gesture Threshold line
+        cv2.line(img, (0, gestureThreshold), (width, gestureThreshold), (0, 255, 0), 10)
+
+        if hands and buttonPressed is False:  # If hand is detected
+            hand = hands[0]
+            cx, cy = hand["center"]
+            lmList = hand["lmList"]  # List of 21 Landmark points
+            fingers = detectorHand.fingersUp(hand)  # List of which fingers are up
+
+            # Constrain values for easier drawing
+            xVal = int(np.interp(lmList[8][0], [width // 2, width], [0, width]))
+            yVal = int(np.interp(lmList[8][1], [150, height - 150], [0, height]))
+            indexFinger = xVal, yVal
+
+            if cy <= gestureThreshold:  # If hand is at the height of the face
+                if fingers == [1, 0, 0, 0, 0]:
+                    print("Left")
+                    buttonPressed = True
+                    if imgNumber > 0:
+                        imgNumber -= 1
+                        annotations = [[]]
+                        annotationNumber = -1
+                        annotationStart = False
+
+            elif fingers == [0, 0, 0, 0, 1]:
+                print("Right")
+                buttonPressed = True
+                if imgNumber < len(pathImages) - 1:
+                    imgNumber += 1
+                    annotations = [[]]
+                    annotationNumber = -1
+                    annotationStart = False
+            
+            if fingers == [0, 1, 1, 0, 0]:
+                cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
+
+            if fingers == [0, 1, 0, 0, 0]:
+                if annotationStart is False:
+                    annotationStart = True
+                    annotationNumber += 1
+                    annotations.append([])
+                annotations[annotationNumber].append(indexFinger)
+                cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
+
+            else:
+                annotationStart = False
+
+            if fingers == [0, 1, 1, 1, 0]:
+                if annotations:
+                    annotations.pop(-1)
+                    annotationNumber -= 1
+                    buttonPressed = True
+
+        else:
+            annotationStart = False
+
+        if buttonPressed:
+            counter += 1
+            if counter > delay:
+                counter = 0
+                buttonPressed = False
+
+        # Draw annotations on the current image
+        for i, annotation in enumerate(annotations):
+            for j in range(len(annotation)):
+                if j != 0:
+                    cv2.line(imgCurrent, annotation[j - 1], annotation[j], (0, 0, 200), 12)
+
+        # Show the camera feed and the current image
+        imgSmall = cv2.resize(img, (ws, hs))
+        h, w, _ = imgCurrent.shape
+        imgCurrent[0:hs, w - ws:w] = imgSmall
+
+        ret, buffer = cv2.imencode('.jpg', imgCurrent)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -15,10 +137,23 @@ def home():
             for file in uploaded_files:
                 # Save the uploaded file to the 'presentation' folder
                 file.save(os.path.join('presentation', file.filename))
+            # Update the pathImages list after uploading new files
+            pathImages = sorted(os.listdir(folderPath), key=len)
             return redirect(url_for('present'))
     return render_template('index.html')
 
 @app.route('/present')
 def present():
-    pathImages = sorted(os.listdir('presentation'), key=len)
     return render_template('presentation.html', images=pathImages)
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    # Start the gesture detection code in a separate thread
+    gesture_thread = threading.Thread(target=generate_frames)
+    gesture_thread.start()
+
+    # Run the Flask app
+    app.run(debug=True)
